b206db783bb48b5294a7686d3d33d7de523af387
rares01 <79113662+rares01@users.noreply.github.com>
2021-12-28 17:12:49 +0200

    added functions for left swipe and logic

--- GameOf2048/main.py
+++ GameOf2048/main.py
@@ -10,7 +10,7 @@ class GameOf2048:
     a = 0
 
     def __init__(self, diff):
-        self.matrix = np.zeros((n, m))
+        self.matrix = np.zeros((n, m), dtype=int)
         self.difficulty = diff
 
     def putANewNumber(self, k=1):
@@ -34,11 +34,41 @@ class GameOf2048:
     def __str__(self):
         return str(self.matrix)
 
-    # def new_move(self,move):
+    @staticmethod
+    def get_numbers(row_pos):
+        numbers = row_pos[row_pos != 0]
+        sum_of_numbers = []
+        ok = False  # for skipping
+        for j in range(len(numbers)):
+            if ok:
+                ok = False
+                continue
+            if j != len(numbers) - 1 and numbers[j] == numbers[j + 1]:  # if we can add 2 numbers that are neighbours
+                # on the same row
+                new_number = numbers[j] * 2
+                ok = True
+            else:
+                new_number = numbers[j]
+
+            sum_of_numbers.append(new_number)
+        return np.array(sum_of_numbers)
+
+    def new_move(self, move):
+        for i in range(n):
+            row_pos = self.matrix[i, :]  # every number from the row
+            numbers = self.get_numbers(row_pos)
+
+            copy = np.zeros_like(row_pos)
+            copy[:len(numbers)] = numbers
+            self.matrix[i, :] = copy  # moving everything to left
 
 
 if __name__ == '__main__':
     difficulty = str(input())
     start = GameOf2048(difficulty)
     start.putANewNumber(k=2) # firstly we take 2 poistions becuase that  is how the game starts
+    print(start)
+    start.putANewNumber(k=2)
+    print(start)
+    start.new_move(move='l')
     print(start)
