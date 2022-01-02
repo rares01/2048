343074cc38e73076ba7c8e6699b5f91cb262e3f8
rares01 <79113662+rares01@users.noreply.github.com>
2022-01-03 00:34:47 +0200

    added graphics implementation and quit game verification for game

--- GameOf2048/main.py
+++ GameOf2048/main.py
@@ -1,17 +1,72 @@
 import random
-
+import pygame
+from pygame.locals import *
 import numpy as np
 
-n = 4
-m = 4
+colors = {
+    'backgorund': (189, 172, 161),
+    0: (204, 192, 179),
+    2: (238, 228, 218),
+    4: (237, 224, 200),
+    8: (242, 177, 121),
+    16: (245, 149, 99),
+    32: (246, 124, 95),
+    64: (246, 94, 59),
+    128: (237, 207, 114),
+    256: (237, 204, 97),
+    512: (237, 200, 80),
+    1024: (237, 197, 63),
+    2048: (237, 194, 46),
 
+}
+
+
+# n mai mare pcia sus jos
+# m mai mare pica drpt stanga
 
 class GameOf2048:
     a = 0
 
     def __init__(self, diff):
-        self.matrix = np.zeros((n, m), dtype=int)
+        self.n = 4
+        self.m = 5
+        self.matrix = np.zeros((self.n, self.m), dtype=int)
+        self.spacing = 10
         self.difficulty = diff
+        self.w = 500
+        self.h = 500
+        pygame.init()
+        pygame.display.set_caption("2048")
+        pygame.font.init()
+        self.myFont = pygame.font.SysFont('Comic Sans MS', 30)
+
+        self.screen = pygame.display.set_mode((self.w, self.h))
+
+    def print_game(self):
+        self.screen.fill(colors['backgorund'])
+
+        for i in range(self.n):
+            for j in range(self.m):
+                num = self.matrix[i][j]
+                if self.m >= self.n:
+                    x_axis = j * self.w // self.m + self.spacing
+                    y_axis = i * self.h // self.n + self.spacing
+                    width = self.w // self.m - 2 * self.spacing
+                    height = self.h // self.m - 2 * self.spacing
+                elif self.n > self.m:
+                    x_axis = j * self.w // self.m + self.spacing
+                    y_axis = i * self.h // self.n + self.spacing
+                    width = self.w // self.n - 2 * self.spacing
+                    height = self.h // self.n - 2 * self.spacing
+
+                pygame.draw.rect(self.screen, colors[num], pygame.Rect(x_axis, y_axis, height, width),
+                                 border_radius=8)
+
+                if num == 0:
+                    continue
+                text = self.myFont.render(f'{num}', True, (0, 0, 0))
+                rect_text = text.get_rect(center=(x_axis + width / 2, y_axis + height / 2))
+                self.screen.blit(text, rect_text)
 
     def putANewNumber(self, k=1):
         positions = list(zip(*np.where(self.matrix == 0)))  # positions from matrix where we have 0
@@ -36,7 +91,7 @@ class GameOf2048:
 
     @staticmethod
     def get_numbers(row_pos):
-        numbers = row_pos[row_pos != 0]
+        numbers = row_pos[row_pos != 0]  # numbers different from 0
         sum_of_numbers = []
         ok = False  # for skipping
         for j in range(len(numbers)):
@@ -54,12 +109,15 @@ class GameOf2048:
         return np.array(sum_of_numbers)
 
     def new_move(self, move):
-        for i in range(n):
+        global row_pos
+        ok = 0
+
+        for i in range(self.n):
             if move in 'lr':
                 row_pos = self.matrix[i, :]  # every number from the row
-            else:
-                row_pos = self.matrix[:, i]  # every number from the col
-
+                ok = 1
+            else :
+                break
             switch = False
             if move in 'rd':
                 switch = True
@@ -75,22 +133,78 @@ class GameOf2048:
 
             if move in 'lr':
                 self.matrix[i, :] = copy
-            else :
-                self.matrix[:, i] = copy
 
+        if ok == 0:
+            for i in range(self.m):
+                if move in 'du':
+                    row_pos = self.matrix[:, i]  # every number from the col
+
+                switch = False
+                if move in 'rd':
+                    switch = True
+                    row_pos = row_pos[::-1]  # we flip the list
+                # why we flip ? It is easy: when we take the numbers in horizontal way
+                numbers = self.get_numbers(row_pos)
+
+                copy = np.zeros_like(row_pos)
+                copy[:len(numbers)] = numbers
+
+                if switch:
+                    copy = copy[::-1]
+                if move in 'du':
+                    self.matrix[:, i] = copy
+
+    def isOver(self):
+        copy_matrix = self.matrix.copy()
+        for move in 'lrdu':
+            self.new_move(move)
+            if not all((self.matrix == copy_matrix).flatten()):
+                self.matrix = copy_matrix
+                return False
+        return True
+
+    @staticmethod
+    def key():
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    return 'q'
+                if event.type == KEYDOWN:
+
+                    if event.key == K_w:
+                        print(1)
+                        return 'u'
+
+                    if event.key == K_s:
+                        return 'd'
+
+                    if event.key == K_a:
+                        return 'l'
+
+                    if event.key == K_d:
+                        return 'r'
 
     def play(self):
         self.putANewNumber(k=2)
         while True:
             print(self.matrix)
-            cmd = input()
+            self.print_game()
+            pygame.display.flip()
+            cmd = self.key()
             if cmd == 'q':
                 break
             past_matrix = self.matrix.copy()
             self.new_move(cmd)
-            if all((self.matrix == past_matrix).flatten()):
-                continue
+            # if all((self.matrix == past_matrix).flatten()):
+            # continue
+
+            if self.isOver():
+                print("Game over!")
+                break
             self.putANewNumber()
+
+
 
 
 if __name__ == '__main__':
